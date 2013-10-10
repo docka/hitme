41a3c7d7de7111d012db4d43d966e9630b5f6006
Tue Rossel <tuerossel@gmail.com>
2013-10-10 15:49:06 +0200

    Added geolocation (still just to console though) I works locally in Firefox, but not Chrome (something with localhost), so I'm trying to put it on github to see if I can get it to work in Chrome.

--- js/user.js
+++ js/user.js
@@ -23,4 +23,66 @@ $(document).ready(function() {
 	 	$("#messageDiv")[0].scrollTop = $("#messageDiv")[0].scrollHeight;
 	 }
 
+/*
+
+var x=document.getElementById("demo");
+function getLocation()
+  {
+  if (navigator.geolocation)
+    {
+    navigator.geolocation.getCurrentPosition(showPosition);
+    }
+  else{x.innerHTML="Geolocation is not supported by this browser.";}
+  }
+function showPosition(position)
+  {
+  x.innerHTML="Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;	
+  }
+
+
+*/
+
+
+	$("#findGeo").on("click", function(event){
+		
+
+		if(navigator.geolocation){
+			console.log("yes, I have geolocation");
+			geo = navigator.geolocation;
+			if (geo){
+				geo.getCurrentPosition(showPosition, function(){
+					//handle error
+					console.log("PositionError code: " + e.code + ": " + e.message);
+				});
+				
+			}
+			else{
+				console.log("geo is null");
+			}
+
+		}
+		else{
+			console.log("geolocation not supported");
+		}
+	//navigator.geolocation.getCurrentPosition(GetLocation);
+	
+	});
+
+
+	function showPosition(position)
+	  {
+	  
+	  var coord_lat = position.coords.latitude;
+	  var coord_long = position.coords.longitude;
+	  var howAccurate = position.coords.accuracy;
+
+	  console.log("Latitude: " + coord_lat + ", longitude: " + coord_long + ", accuracy: " + howAccurate + " meters.");
+	  }
+
+	
+
+
+    
+
 });
