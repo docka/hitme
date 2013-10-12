41e7d28e2f1a4654945eed787e6a554498d20b53
Tue Rossel <tuerossel@gmail.com>
2013-10-12 22:13:46 +0200

    Lots of development both user + business side.

--- js/biz.js
+++ js/biz.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+
+    /* THIS BLOCK IS LISTENING FOR CHAT MESSAGES TO THE BIZ
 	var myDataRef = new Firebase("https://hitme1.firebaseio.com/");
 
 
@@ -19,6 +21,91 @@ $(document).ready(function() {
 	 function displayChatMessage(name,text){
 	 	$("<div/>").text(text).prepend($("<em/>").text(name+": ")).appendTo($("#requestDiv"));
 	 	$("#requestDiv")[0].scrollTop = $("#requestDiv")[0].scrollHeight;
-	 }
+	 }*/
 
+
+    //THIS BLOCK IS LISTENING FOR FOOD REQUESTS
+    var myDataRef = new Firebase("https://hitme-food.firebaseio.com/food");
+
+     myDataRef.on("child_added", function(snapshot){
+
+        var message = snapshot.val();
+        //console.log(message);
+        var html = "";
+        var deadline = makeDeadline(message.timespan, message.timestamp);
+        if (deadline > new Date()){
+            $("#foodRequests").append("<hr />");
+            if (message.category === "food"){
+                html = "<div class='row'>";
+                html += "<div class='col-lg-1'>" + message.numPeople + "</div>";
+                html += "<div class='col-lg-2'>" + deadline.toString() + "</div>";
+                html += "<div class='col-lg-2'>" + message.userID + "</div>";
+                html += "<div class='col-lg-2'>" + message.latitude + "</div>";
+                html += "<div class='col-lg-2'>" + message.longitude + "</div>";
+                html += "<button type='button' class='sendoffers' id='" + message.userID + "'>Send offer</button>"
+                html += "</div>";
+
+                $("#foodRequests").append(html);
+                //displayChatMessage(message.numPeople, message.text); 
+            }
+            else{
+                // not for me .. displayChatMessage("anonymous", "not for me");
+                console.log("Oops, something's wrong with the category.");
+            }
+
+        }
+
+        else{
+            //this request is in the past and will be ignored
+
+        }
+        
+      });
+
+     //SEND OFFERS
+    $(document).on("click", ".sendoffers", function(){
+        console.log(this.id);
+
+
+    });
+
+
+    //adds the timespan to the timestamp = the endpoint of the user's interest in offers
+    function makeDeadline(extraMinutes, startTime){        
+        var start = new Date(startTime); //in milliseconds
+        return new Date(start.getTime() + extraMinutes*60000);
+     }
+
+
+
+     //var responseRef = new Firebase("https://hitme-food.firebaseio.com/messages/89234jhkasdf(#4jejrhwerjkwr");
+  
+
+
+
+     //RETRIEVE FOOD REQUESTS FROM SERVER
+     $("#getRequests").on("click",function(){
+        var myDataFoodRef = new Firebase("https://hitme-food.firebaseio.com/");
+        myDataFoodRef.once("value", showData, handleFailure);
+
+
+     });
+
+
+
+
+     function showData(snapshot){
+        snapshot.forEach(function(childSnapshot){
+            var data = childSnapshot.val();
+            console.log(data.category);
+        });
+
+        //console.log("Snapshot value: " + x.val());
+
+     }
+
+     function handleFailure(){
+
+            console.log("in handleFailure");
+     }
 });
--- js/user.js
+++ js/user.js
@@ -1,88 +1,171 @@
 $(document).ready(function() {
+	  //global variables
+	  var map; 
+	  var marker; //the dot on the map
+	  var lat;
+	  var lon;
+	  var userID;
 
-	var myDataRef = new Firebase("https://hitme1.firebaseio.com/");
-		$("#messageInput").keypress(function(e){
-			if (e.keyCode == 13){
-				var name = $("#nameInput").val();
-				var text = $("#messageInput").val();
-				myDataRef.push({name: name, text: text});
-				$("#messageInput").val("");
+	  checkCookie(); //load or create cookie for identification
 
-			}
+	  loadGMaps(); //load map
 
-		});
+	  
+	  function checkCookie(){
+	  	
+	  	var cooc = document.cookie; 
+	  	
+	  	if(cooc!=null && cooc!=""){
+	  		//yes
+	  		//console.log("Cookie exists: " + cooc);
+	  		userID = cooc.substring(cooc.indexOf("=") + 1,cooc.length);
+	  		console.log("Userid: " + userID);
+	  	}
+	  	else{
+	  		//no
+	  		console.log("No cookie. Creating one now.");
+	  		//setting new cookie, expiring tomorrow
+	  		var exdate = new Date();
+	  		exdate.setDate(exdate.getDate() + 1);
+	  		document.cookie = "biddit_user=" + Math.random() + "; expires=" + exdate.toUTCString();
+	  	}
+	  }
 
-	//listener for incoming messages
-	 myDataRef.on("child_added", function(snapshot){
+
+		//LISTENING FOR INCOMING MESSAGES FROM FIREBASE
+	 /* myDataRef.on("child_added", function(snapshot){
      	var message = snapshot.val();
      	displayChatMessage(message.name, message.text); 
-      });
+      }); */
 
+		/*
 	 function displayChatMessage(name,text){
 	 	$("<div/>").text(text).prepend($("<em/>").text(name+": ")).appendTo($("#messageDiv"));
 	 	$("#messageDiv")[0].scrollTop = $("#messageDiv")[0].scrollHeight;
-	 }
-
-/*
-
-var x=document.getElementById("demo");
-function getLocation()
-  {
-  if (navigator.geolocation)
-    {
-    navigator.geolocation.getCurrentPosition(showPosition);
-    }
-  else{x.innerHTML="Geolocation is not supported by this browser.";}
-  }
-function showPosition(position)
-  {
-  x.innerHTML="Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;	
-  }
+	 }*/
 
 
-*/
+	//GATHER DATA AND SEND TO FIREBASE AS JSON-OBJECT
+	$("#createObject").on("click", validateInput);
+
+	function validateInput(){
+		//check if everything is alright
+		var cat = $("input[name=optionsCategory]:checked").val();
+		var nPeople = $("#numPeople")[0].value;
+		var minutes = $("#timeSpan")[0].value;
+		if(userID == null){
+			checkCookie();
+		}
+
+		sendToServer(cat, nPeople, lat, lon, minutes);
+	}
+
+	function sendToServer(category, people, latitude, longitude, minutes){
+		var timestamp = new Date;
+		var milliDate = timestamp.getTime();
+
+	
+		try
+		  {
+		  	var myFoodDataRef = new Firebase("https://hitme-food.firebaseio.com/" + category);
+		  	myFoodDataRef.push(
+				{
+					category: category,
+					numPeople: people,
+					timespan: minutes,
+					latitude: latitude,
+					longitude: longitude,
+					timestamp: milliDate,
+					userID: userID
+				}
+			);
+		  }
+		catch(err)
+		  {
+		  	console.log("Couldn't write to Firebase: " + err.message);
+		  }
+		}
 
 
-	$("#findGeo").on("click", function(event){
+
+
+	
+
+
+	//google map
+	function loadGMaps(){
+
+ 	 	if(navigator.geolocation){
+			navigator.geolocation.getCurrentPosition(showLocation, function(e){
+				console.log("PositionError code: " + e.code + ": " + e.message);
+				showLocation(null);
+
+			}); 
+		}
+
+		else{
+			console.log("Geolocation not supported");
+		}
+
+	}
+
+	//if geolocation was a success, find and show position
+	function showLocation(position){
+		//var lat;
+		//var lon;
+		var accu;
+		var map_options;
+		var txt;
+		var zoomLevel;
 		
 
-		if(navigator.geolocation){
-			console.log("yes, I have geolocation");
-			geo = navigator.geolocation;
-			if (geo){
-				geo.getCurrentPosition(showPosition, function(){
-					//handle error
-					console.log("PositionError code: " + e.code + ": " + e.message);
-				});
-				
-			}
-			else{
-				console.log("geo is null");
-			}
-
+		if(position!=null){
+			lat = position.coords.latitude;
+			lon = position.coords.longitude;
+			accu = position.coords.accuracy;
+			zoomLevel = 14;
+			txt = "We have located you to the red marker on the map. If this is way off, just drag the marker and drop it where you really are.";
 		}
 		else{
-			console.log("geolocation not supported");
+			//geolocation not allowed, set default Stockholm position
+			lat = 59.32490206965159;
+			lon = 18.069624362207037;
+			zoomLevel = 12;
+			txt = "Your browser doesn't allow us to find you automatically. Please drag the marker to where you really are before continuing.";
 		}
-	//navigator.geolocation.getCurrentPosition(GetLocation);
-	
-	});
+
+		//make map
+		map_options = {
+			center: new google.maps.LatLng(lat,lon),
+			zoom: zoomLevel,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		}
+		$("#whereareyou").html(txt);
+		
+		map = new google.maps.Map($('#map_canvas')[0], map_options); //don't understand the need for [0], but it won't work without it
+
+		var myLatlng = new google.maps.LatLng(lat,lon);
+
+		marker = new google.maps.Marker({
+		      position: myLatlng,
+		      map: map,
+		      draggable: true,
+		      title: 'You are here!'
+		});
+		google.maps.event.addListener(marker, "mouseup", function(){
+			lat = marker.getPosition().lat();
+			lon = marker.getPosition().lng();
+			moveToLocation(lat,lon);
+		}); 
+
+		//RE-CENTER THE MAP
+		function moveToLocation(lat, lon){
+    		var center = new google.maps.LatLng(lat, lon);
+   			map.panTo(center);
+}
 
 
-	function showPosition(position)
-	  {
-	  
-	  var coord_lat = position.coords.latitude;
-	  var coord_long = position.coords.longitude;
-	  var howAccurate = position.coords.accuracy;
-
-	  console.log("Latitude: " + coord_lat + ", longitude: " + coord_long + ", accuracy: " + howAccurate + " meters.");
-	  }
-
-	
-
-
-    
+	}
+	   
 
 });
