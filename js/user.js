e197cba6aecda8ddc964987ff26173abc18f374c
Tue Rossel <tuerossel@gmail.com>
2013-10-15 21:26:10 +0200

    The user-side has been heavily redone in four steps (sections made visible/invisible).

--- js/user.js
+++ js/user.js
@@ -6,38 +6,15 @@ $(document).ready(function() {
 	  var lon;
 	  var userID;
 	  var numPersons;
+	  var minutes = 15; //default waiting time - updated in changeTime()
 
 
 // SL API KEY 0684fdd18500c8c13433086c0dbd9943
 
-	//BUTTONS FADING
-
-
-	$("#chooseFood").on("click", function(){
-		$("#chooseEvent").fadeOut();
-		$("#chooseBars").fadeOut();
-		$("#chooseOther").fadeOut();
-		$("#secondRow").show();
-
-		
-
-
-	});
-
-	$("#chooseEvent").on("click", function(){
-		$("#chooseFood").fadeOut();
-		$("#chooseBars").fadeOut();
-		$("#chooseOther").fadeOut();
-
-	});
-
-
-
-
 	  checkCookie(); //load or create cookie for identification
 
 
-	  loadGMaps(); //load map
+	
 
 	  
 	  function checkCookie(){
@@ -63,19 +40,28 @@ $(document).ready(function() {
 
 
 	//GATHER DATA AND SEND TO FIREBASE AS JSON-OBJECT
-	$("#createObject").on("click", validateInput);
+	//$("#createObject").on("click", validateInput);
 
 	function validateInput(){
 		//check if everything is alright
 		var cat = "food"; //"$("input[name=optionsCategory]:checked").val();"
 		var nPeople = numPersons; //$("#numPeople")[0].value;
-		var minutes = $("#timeSpan")[0].value;
+		//var minutes = $("#timeSpan")[0].value;
+
+
 		if(userID == null){
 			checkCookie();
 		}
 
+		console.log("Creating object on server____________");
+		console.log("cat: " + cat);
+		console.log("nPeople: " + nPeople);
+		console.log("latitude: " + lat);
+		console.log("long: " + lon);
+		console.log("minutes: " + minutes);
+
 		sendToServer(cat, nPeople, lat, lon, minutes);
-		//window.location.href = "offerlist.html";
+		
 	}
 
 	function sendToServer(category, people, latitude, longitude, minutes){
@@ -135,23 +121,23 @@ $(document).ready(function() {
 	function showLocation(position){
 		var accu;
 		var map_options;
-		var txt;
+		var txt = "Please drag the marker to the area where you want to eat.";
 		var zoomLevel;
 		
-
+		//console.log("I'm in showlocation");
 		if(position!=null){
 			lat = position.coords.latitude;
 			lon = position.coords.longitude;
 			accu = position.coords.accuracy;
 			zoomLevel = 14;
-			txt = "We have located you to the red marker on the map. If this is way off, just drag the marker and drop it where you really are.";
+			//txt = "Please drag the marker to the area where you want to eat.";
 		}
 		else{
 			//geolocation not allowed, set default Stockholm position
 			lat = 59.32490206965159;
 			lon = 18.069624362207037;
 			zoomLevel = 12;
-			txt = "Your browser doesn't allow us to find you automatically. Please drag the marker to where you really are before continuing.";
+			//txt = "Your browser doesn't allow us to find you automatically. Please drag the marker to where you really are before continuing.";
 		}
 
 		//make map
@@ -214,13 +200,35 @@ $(document).ready(function() {
 		
 	});
 
-	//CLICKING ON PERSONS
+
+	//////////////////////////////////////////
+	//										//	
+	//		HANDLE THE SECTION CLICKING		//
+	//										//
+	//////////////////////////////////////////
+
+	//FIRST SECTION 
+	//choosing the category - so far food is the only one that works
+	$("#chooseFood").on("click", function(){
+		
+		//$("#chooseEvent").fadeOut();
+		//$("#chooseBars").fadeOut();
+		//$("#chooseOther").fadeOut();
+		$("#firstSection").fadeOut(800, function(){
+			$("#secondHeader").prepend("Food it is. ");
+			$("#secondSection").fadeIn(800);		
+		});
+	});
+
+
+	//SECOND SECTION
+	//Clicking on the circle of persons
 	$(".persons").on("click", function(){
 		
 		var currentId = parseInt(this.id.substring(7, this.id.length));
 		numPersons= currentId;
 		var nextId = "person_";
-		$("#amountPeople").html("<p id='circleText'>" + currentId + "</p>");
+		//$("#amountPeople").html("<p id='circleText'>" + currentId + "</p>");
 		
 		
 		//going down, colouring white
@@ -237,22 +245,157 @@ $(document).ready(function() {
 		
 	});
 
-	//HOVERING ON CIRCLE INSIDE PERSONS
+	//Hovering on the circle of persons
 	$("#amountPeople").on("mouseover", function(){
 		//var number = $("#circleText").html();
 		//$("#amountPeople").html(number + " <p>Click</p>");
 					
 	});
-	//CLICKING ON THE NUMBER OF PEOPLE
+
+	//Clicking on the circle INSIDE the circle of persons
 	$("#amountPeople").on("click", function(){
-		console.log("clicked");
+		$("#secondSection").fadeOut(800, function(){
+			$("#thirdSection").fadeIn(800);
+
+		});
 	
 		//var number = $("#amountPeople").html();
 		//$("#amountPeople").html(number + " <p>Click</p>");
 					
 	});
 
-	
+	//THIRD SECTION
+	$("#timeSubtract").on("click", function(){
+		changeTime("subtract");
+	});
+
+	$("#timeAdd").on("click", function(){
+		changeTime("add");
+	});
+
+
+	function changeTime(direction){
+		var strTime = $("#textTime").html();
+		var denominator = strTime.substring(strTime.length-1, strTime.length);
+		var number = parseInt(strTime.substring(0, strTime.length-1));
+
+		switch(number){
+			case 15:
+				if(direction==="add"){
+					$("#textTime").html("30m");
+					minutes = 30;
+				}
+				else
+				{
+					//do nothing, can't go below 15 minutes
+				}
+
+			break;
+
+			case 30:
+				if(direction==="add"){
+					$("#textTime").html("45m");
+					minutes = 45;
+				}
+				else
+				{
+					$("#textTime").html("15m");
+					minutes = 15;
+				}
+
+			break;
+
+			case 45:
+				if(direction==="add"){
+					$("#textTime").html("1h");
+					minutes = 60;
+				}
+				else
+				{
+					$("#textTime").html("30m");
+					minutes = 30;
+				}
+			break;
+
+			case 1:
+				if(direction==="add"){
+					$("#textTime").html("2h");
+					minutes = 120;
+				}
+				else
+				{
+					$("#textTime").html("45m");
+					minutes = 45;
+				}
+			break;
+
+			case 2:
+				if(direction==="add"){
+					$("#textTime").html("3h");
+					minutes = 180;
+				}
+				else
+				{
+					$("#textTime").html("1h");
+					minutes = 60;
+				}
+			break;
+
+			case 3:
+				if(direction==="add"){
+					$("#textTime").html("4h");
+					minutes = 240;
+				}
+				else
+				{
+					$("#textTime").html("2h");
+					minutes = 120;
+				}
+
+			break;
+
+			case 4:
+				if(direction==="add"){
+					$("#textTime").html("5h");
+					minutes = 300;
+				}
+				else
+				{
+					$("#textTime").html("3h");
+					minutes = 180;
+				}
+			break;
+
+			case 5:
+				if(direction==="add"){
+					//do nothing, can't go above 5 hours
+				}
+				else
+				{
+					$("#textTime").html("4h");
+					minutes = 240;
+				}
+			break;
+
+			default:
+				console.log("faulty switch");
+
+		}
+		
+	}
+
+
+	$("#textTime").on("click", function(){
+		$("#thirdSection").fadeOut(800, function(){
+			$("#fourthSection").fadeIn(800);	
+			loadGMaps(); //load map	
+		});
+
+	});
+
+	//FOURTH SECTION
+
+		$("#createObject").on("click", validateInput);
 	   
 
 
